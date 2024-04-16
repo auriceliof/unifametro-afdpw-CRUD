921f0cbcee9ba9c645f6cc7c99fe3e96da077395
auriceliof <auricelio.suporte@hotmail.com>
2024-04-15 22:28:16 -0300

    Sort by ID

--- frontend/src/service/student-service.ts
+++ frontend/src/service/student-service.ts
@@ -3,7 +3,7 @@ import { BASE_URL } from "../utils/system";
 import { requestBackend } from "../utils/requests";
 import { StudentDTO } from "../models/student";
 
-export function findPageRequest(page: number, name: string, size = 12, sort = "name") {
+export function findPageRequest(page: number, name: string, size = 12, sort = "id") {
     const config : AxiosRequestConfig = {
         method: "GET",
         baseURL: BASE_URL,
