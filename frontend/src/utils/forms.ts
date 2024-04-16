35856174235a4687410f2a1ad7aa0b81e150ba41
auriceliof <auricelio.suporte@hotmail.com>
2024-04-15 21:39:31 -0300

    Validation inputs: name and cpf

--- frontend/src/utils/forms.ts
+++ frontend/src/utils/forms.ts
@@ -2,3 +2,26 @@ export function update(inputs: any, name: string, newValue: any) {
     return{ ...inputs, [name]: { ...inputs[name], value: newValue} };
 }
 
+export function updateAll(inputs: any, newValues: any) {
+
+    const newInputs: any = {};
+    for (var name in inputs) {
+        newInputs[name] = { ...inputs[name], value: newValues[name] };
+    }
+    return newInputs;
+}
+
+export function validate(inputs: any, name: string) {
+
+    if (!inputs[name].validation) {
+        return inputs
+    }
+
+    const isInvalid = !inputs[name].validation(inputs[name].value);
+    return { ...inputs, [name]: { ...inputs[name], invalid: isInvalid.toString() }};
+}
+
+export function updateAndValidate(inputs: any, name: string, newValue: any) {
+    const dataUpdated = update(inputs, name, newValue);
+    return validate(dataUpdated, name);
+}
