6f3b99e78c459d9b8029477eef404b75eb31cc48
auriceliof <auricelio.suporte@hotmail.com>
2024-04-16 10:41:01 -0300

    checkpoint2

--- demo-backend-springboot-jpa/src/main/java/br/fametro/xpto/v1/repository/RepositorioAluno.java
+++
@@ -1,9 +0,0 @@
-package br.fametro.xpto.v1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.fametro.xpto.v1.model.Aluno;
-
-public interface RepositorioAluno extends JpaRepository<Aluno, Long> {
-    // Métodos customizados podem ser declarados aqui
-}
