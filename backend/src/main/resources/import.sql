836e909f727f0537cc05bd99af1d062020548dfc
auriceliof <auricelio.suporte@hotmail.com>
2024-04-17 11:57:16 -0300

    Refactor DATE in backend

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -2,7 +2,3 @@ INSERT INTO tb_student (name, cpf, birth_date, income) VALUES ('Auricelio Freita
 INSERT INTO tb_student (name, cpf, birth_date, income) VALUES ('Miguel Soares', '000.111.222-33', '1988-09-24', 20700.5);
 INSERT INTO tb_student (name, cpf, birth_date, income) VALUES ('Matheus Marques', '000.111.222-33', '1990-03-21', 10700.5);
 INSERT INTO tb_student (name, cpf, birth_date, income) VALUES ('Rafael Sales', '000.111.222-33', '1991-01-10', 10700.5);
-INSERT INTO tb_student (name, cpf, birth_date, income) VALUES ('Bruno Freitas', '000.111.222-33', '1989-08-25', 9700.5);
-INSERT INTO tb_student (name, cpf, birth_date, income) VALUES ('Romulo Araujo', '000.111.222-33', '1985-02-11', 8700.5);
-INSERT INTO tb_student (name, cpf, birth_date, income) VALUES ('Ryan Sousa', '000.111.222-33', '1984-06-23', 7700.5);
-INSERT INTO tb_student (name, cpf, birth_date, income) VALUES ('test1', '000.111.222-33', null, 1024.0);
